447b5b6f98a8ca989f82ef1fd7b88ef7980ab5ea
Edwin Hernandez <edwin.ejhnsn@gmail.com>
2022-11-12 00:11:34 -0500

    subiendo proyecto js html css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# GAME_ISLA_TESORO_JS
+# ISLA_DEL_TESORO_JS_GAME
+Aca podras jugar y ver mi trabajo --->
+  - Pase mi diagrama de flujo a python y ahora lo quise llevar mas alla, realizando un juego dinamico usando las herramientas de HTML/CSS/JS
+ 
---
+++ script.js
@@ -0,0 +1,174 @@
+// var boton = document.querySelector('.boton');
+// var navegador = document.querySelector ('.navegador');
+
+// boton.addEventListener('click', function(){
+//     navegador.classList.toggle('.activo')
+
+// })
+
+// function toggleMenu(){
+//  var navegador = document.querySelector (".navparaphone");
+//  var height = navegador.scrollHeight;
+ 
+//     if(height === 245 ) {
+//         navegador.style.height = "0";
+//     } else{
+//         navegador.style.height = "245px";
+//     }
+// }
+
+// let pregunta_3 = document.getElementById("pregunta2izq");
+// let pregunta_4 = document.getElementById("pregunta4");
+// let pregunta_5 = document.getElementById("pregunta5");
+// let pregunta_6 = document.getElementById("pregunta6");
+// let pregunta_7 = document.getElementById("pregunta7");
+
+
+
+// @@@@@@@@@ PRIMERA PREGUNTA @@@@@@@@@@@@
+let pregunta_0 = document.getElementById("pregunta0");
+let pregunta_1 = document.getElementById("pregunta1");
+let pregunta_2 = document.getElementById("pregunta2der");
+let pregunta_3 = document.getElementById("pregunta2izq");
+let pregunta_4 = document.getElementById("pregunta3ama");
+let pregunta_5 = document.getElementById("pregunta3azu");
+let pregunta_6 = document.getElementById("pregunta3bla");
+let pregunta_7 = document.getElementById("pregunta4tib");
+let pregunta_8 = document.getElementById("pregunta4ang");
+let pregunta_9 = document.getElementById("pregunta4nad");
+let pregunta_10 = document.getElementById("pregunta4espe");
+let pregunta_11 = document.getElementById("pregunta4pro");
+let pregunta_12 = document.getElementById("pregunta4paz");
+let pregunta_13 = document.getElementById("pregunta4pver");
+let pregunta_14 = document.getElementById("pregunta6mov");
+let pregunta_15 = document.getElementById("pregunta6esca");
+
+function start(){
+    pregunta_0.style.display="none";
+    pregunta_1.style.display="block";
+}
+
+
+
+function derecha1(){
+    pregunta_2.style.display="block";
+    pregunta_1.style.display="none";
+    pregunta_3.style.display="none";
+}
+
+function izquierda1(){
+    pregunta_3.style.display="block";
+    pregunta_2.style.display="none";
+    pregunta_1.style.display="none";
+}
+
+
+// @@@@@@@@@@@@ COLOR DEL HUEVO
+function amarillo2der(){
+    pregunta_6.style.display="none";
+    pregunta_5.style.display="none";
+    pregunta_3.style.display="none";
+    pregunta_2.style.display="none";
+    pregunta_1.style.display="none";
+    pregunta_4.style.display="block";
+}
+
+    function tiburones(){
+        pregunta_4.style.display="none";
+        pregunta_8.style.display="none";
+        pregunta_6.style.display="none";
+        pregunta_5.style.display="none";
+        pregunta_3.style.display="none";
+        pregunta_2.style.display="none";
+        pregunta_1.style.display="none";
+        pregunta_7.style.display="block";
+    }
+
+    function anguilas(){
+        pregunta_4.style.display="none";
+        pregunta_7.style.display="none";
+        pregunta_6.style.display="none";
+        pregunta_5.style.display="none";
+        pregunta_3.style.display="none";
+        pregunta_2.style.display="none";
+        pregunta_1.style.display="none";
+        pregunta_8.style.display="block";
+    }
+
+function azul2der(){
+    pregunta_5.style.display="none";
+    pregunta_4.style.display="none";
+    pregunta_3.style.display="none";
+    pregunta_2.style.display="none";
+    pregunta_1.style.display="none";
+    pregunta_5.style.display="block";
+}
+
+// FALTA TERMINAR LA AZUL 
+
+
+function blanco2der(){
+    pregunta_5.style.display="none";
+    pregunta_4.style.display="none";
+    pregunta_3.style.display="none";
+    pregunta_2.style.display="none";
+    pregunta_1.style.display="none";
+    pregunta_6.style.display="block";
+}
+
+
+function nado1(){
+    pregunta_2.style.display="none";
+    pregunta_3.style.display="none";
+    pregunta_10.style.display="none";
+    pregunta_5.style.display="none";
+    pregunta_9.style.display="block";
+}
+
+
+        function roja3(){
+            pregunta_11.style.display="block";
+            pregunta_9.style.display="none";
+        }
+        function azul3(){
+            pregunta_12.style.display="block";
+            pregunta_9.style.display="none";
+        }
+        function verde3(){
+            pregunta_13.style.display="block";
+            pregunta_9.style.display="none";
+        }   
+        function moverla1(){
+            pregunta_13.style.display="none";
+            pregunta_14.style.display="block";
+            pregunta_15.style.display="none";
+        }
+        function escalar1(){
+            pregunta_13.style.display="none";
+            pregunta_15.style.display="block";
+            pregunta_14.style.display="none";
+        }
+
+
+
+function espero1(){
+    pregunta_2.style.display="none";
+    pregunta_3.style.display="none";
+    pregunta_9.style.display="none";
+    pregunta_5.style.display="none";
+    pregunta_10.style.display="block";
+}
+
+
+
+
+
+
+
+
+
+// @@@@@@@@@@@@@@@@@@@@ SEGUNDA PREGUNTA 
+// DERECHA
+
+
+// IZQUIERDA
